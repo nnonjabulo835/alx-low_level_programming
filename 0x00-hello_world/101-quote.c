af9bcd4c521d1cc363a080f06181707919aca895
nnonjabulo835 <nnonjabulo835@gmail.com>
2023-07-13 19:39:30 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - writes "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-puts(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
