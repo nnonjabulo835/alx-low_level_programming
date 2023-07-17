439aa644969324c8d73fccdaa8686c6ff56de1fa
nnonjabulo835 <nnonjabulo835@gmail.com>
2023-07-17 04:06:41 -0700

    Print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,14 +14,14 @@ num_2 = 0;
 while (num_1 <= 9)
 {
 
-while (num_2 <= 9)
+while (num_2 <= 89)
 {
 
 if (num_1 < num_2)
 {
 putchar(num_1);
 putchar(num_2);
-if (num_1 != 8 || (num_1 == 8 && num_2 != 9))
+if (num_1 != 89 || (num_1 == 89 && num_2 != 90))
 {
 putchar(',');
 putchar(' ');
